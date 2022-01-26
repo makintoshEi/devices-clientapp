11dc0c64b46017a1092fbc7c88541e6c203b0448
Asael <makintosh_ragga@yahoo.es>
2022-01-25 21:09:52 -0500

    feat: Implemented <Filters />

--- src/modules/dashboard/dashboard.loc.tsx
+++ src/modules/dashboard/dashboard.loc.tsx
@@ -1,12 +1,12 @@
 import './dashboard.css'
 import { DataTable } from 'primereact/datatable'
-import { Dropdown } from 'primereact/dropdown'
 import { Column } from 'primereact/column'
 import { Button } from 'primereact/button'
 import { Toast } from 'primereact/toast'
+import { Filters, FiltersProps } from './filters/filters'
 import { DeviceModal, DeviceModalProps } from './device-modal'
 import { Device } from '../../models/device.model'
-import { DASH_CNTS, DEVICE } from './dashboard.constant'
+import { DASH_CNTS } from './dashboard.constant'
 
 export interface DashboardProps {
     devices: any[]
@@ -60,6 +60,14 @@ export const DashboardLoc = (props: DashboardProps) => {
         )
     }
 
+    const filtersProps: FiltersProps = {
+        deviceType,
+        filterDevicesByType,
+        onAddDevice,
+        sortBy,
+        sortDevices
+    }
+
     const deviceModalProps: DeviceModalProps = {
         isCreateFlow,
         isDisabled,
@@ -77,29 +85,7 @@ export const DashboardLoc = (props: DashboardProps) => {
     return (
         <div className="m-6">
             <Toast ref={toast} position="top-right" />
-            <div className="card">
-                <div className="p-fluid formgrid grid">
-                    <div className='field col-12 md:col-4'>
-                        <label className='mr-1' htmlFor="filterBy">{DASH_CNTS.FILTERS.DEVICE.LBL}</label>
-                        <Dropdown name="filterBy" options={DASH_CNTS.DEVICE_TYPES}
-                            onChange={(e) => filterDevicesByType(e.value)}
-                            placeholder={DASH_CNTS.FILTERS.DEVICE.PLH}
-                            value={deviceType} />
-                    </div>
-                    <div className="field col-12 md:col-4">
-                        <label className='mr-1' htmlFor="sortBy">{DASH_CNTS.FILTERS.SORT.LBL}</label>
-                        <Dropdown name="sortBy" options={DASH_CNTS.SORT_TYPES}
-                            onChange={(e) => sortDevices(e.value)}
-                            placeholder={DASH_CNTS.FILTERS.SORT.PLH}
-                            value={sortBy} />
-                    </div>
-                    <div className="field col-1 md:col-3 mt-4">
-                        <Button className="p-button-raised" id="add_device_btn"
-                            label={`${DASH_CNTS.BTN.ADD} ${DEVICE}`}
-                            onClick={onAddDevice} />
-                    </div>
-                </div>
-            </div>
+            <Filters {...filtersProps} />
             <DataTable value={devices} scrollable scrollHeight="500px">
                 <Column field={DASH_CNTS.COLUMNS[0].field} header={DASH_CNTS.COLUMNS[0].header}></Column>
                 <Column field={DASH_CNTS.COLUMNS[1].field} header={DASH_CNTS.COLUMNS[1].header}></Column>
