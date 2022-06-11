72cbfce89e7a0b071c0f1374ed8e392c6a128865
Asael <makintosh_ragga@yahoo.es>
2022-06-10 19:39:37 -0500

    feat: Implemented render-props design pattern

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import './App.css'
 // import { Dashboard } from './modules/dashboard/dashboard'
-import { useState } from 'react'
 import { CustomInput } from './shared/CustomInput'
 import { Farenheit, Kelvin } from './modules/temperature'
 
 function App() {
 
-  const [value, setValue] = useState<string>("");
 
-  const handleChange = (evt: any) => {
-    setValue(evt.target.value)
-  }
   return (
     <div className="App">
-      <CustomInput placeholder='Temp in °C' handleChange={handleChange} value={value} />
-      <Kelvin currentTemperature={value} />
-      <Farenheit currentTemperature={value} />
+      <CustomInput placeholder='Temp in °C'
+        renderProp={(value: string) => (
+          <>
+            <Kelvin currentTemperature={value} />
+            <Farenheit currentTemperature={value} />
+          </>
+        )} />
     </div>
   );
 }
--- src/shared/CustomInput.tsx
+++ src/shared/CustomInput.tsx
@@ -1,17 +1,27 @@
-import { ChangeEventHandler } from "react"
+import { useState } from 'react'
 
 type CustomInputType = {
     placeholder?: string
     type?: string
-    value: any
-    handleChange: ChangeEventHandler
+    renderProp: any
 }
 
 export function CustomInput(props: CustomInputType) {
-    const { handleChange, placeholder, type, value } = props
-    return (<input placeholder={placeholder}
-        type={type}
-        style={{ borderRadius: 4, padding: 16 }}
-        value={value}
-        onChange={handleChange} />);
+
+    const { placeholder, type, renderProp } = props
+    const [value, setValue] = useState<string>("");
+
+    const handleChange = (evt: any) => {
+        setValue(evt.target.value)
+    }
+
+    return (
+        <>
+            <input placeholder={placeholder}
+                type={type}
+                style={{ borderRadius: 4, padding: 16 }}
+                value={value}
+                onChange={handleChange} />
+            {renderProp(value)}
+        </>)
 }
