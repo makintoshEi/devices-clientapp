898a237228e2e77d43ff2a0606d72acb70386d6b
Asael <makintosh_ragga@yahoo.es>
2022-06-16 20:05:10 -0500

    feat: added flex container to .App class

---
+++ src/modules/Temperature.hoc.tsx
@@ -0,0 +1,12 @@
+
+
+export const TemperatureHOC = (WrappedComponent: any) => {
+
+    // const [] = useState<string>()
+    // Fetch API
+
+
+    return (
+        <WrappedComponent></WrappedComponent>
+    )
+}
